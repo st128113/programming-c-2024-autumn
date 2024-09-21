3cde013fe0d1cddaca8681fc976b482062dcec5d
st128113 <st128113@student.spbu.ru>
2024-09-21 23:53:57 +0300

    Update Source.cpp

--- 2024.09.17-Homework-1/Task9/Source.cpp
+++ 2024.09.17-Homework-1/Task9/Source.cpp
@@ -4,18 +4,18 @@ int main(int argc, char* argv[])
     int h = 0;
     int a = 0;
     int b = 0;
-    int day = 0;
+    int d = 0;
     scanf("%d", &h);
     scanf("%d", &a);
     scanf("%d", &b);
     
     h = h + (((a - b) - h % (a - b)) % (a - b));
     h = (h - a) * ((h / a) - ((h - a) / a));
-    day++;
+    d++;
     h = h + (((a - b) - h % (a - b)) % (a - b));
-    day += h / (a - b);
+    d += h / (a - b);
     
-    printf("%d", day);
+    printf("%d", d);
     
     return EXIT_SUCCESS;
 }
