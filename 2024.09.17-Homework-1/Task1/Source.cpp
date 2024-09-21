000e416cf5eecee778f6c0b711b53433e104eeea
st128113 <st128113@student.spbu.ru>
2024-09-21 17:46:23 +0300

    Update Source.cpp

--- 2024.09.17-Homework-1/Task1/Source.cpp
+++ 2024.09.17-Homework-1/Task1/Source.cpp
@@ -5,8 +5,7 @@ int main(int argc, char* argv[])
     int a = 0;
     int b = 0;
     scanf("%d", &a);
-    scanf("%d", &b)
-        ;
+    scanf("%d", &b);
     printf("%d", a + b);
 
     return EXIT_SUCCESS;
