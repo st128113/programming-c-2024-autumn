1794cd1a06055318d7200b86594c945b594d6cc2
st128113 <st128113@student.spbu.ru>
2024-09-21 18:18:20 +0300

    Update Source.cpp

--- 2024.09.17-Homework-1/Task8/Source.cpp
+++ 2024.09.17-Homework-1/Task8/Source.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
     int a = 0;
     scanf("%d", &a);
 
-    int = (a % 10) + (a / 10 % 10) + (a / 100);
+    int s = (a % 10) + (a / 10 % 10) + (a / 100);
     printf("%d", s);
 
     return EXIT_SUCCESS;
