f202d155f5069e72df2b265326d5d7cc26a2b8d8
st128113 <st128113@student.spbu.ru>
2024-09-23 17:06:57 +0300

    Update Source.cpp

--- 2024.09.17-Homework-1/Task9/Source.cpp
+++ 2024.09.17-Homework-1/Task9/Source.cpp
@@ -1,15 +1,35 @@
-#include <stdio.h> 
- 
-int main(int argc, char* argv[]) { 
-    int H, A, B; 
-    scanf("%d", &H); 
-    scanf("%d", &A); 
-    scanf("%d", &B); 
- 
-    int N = A - B; 
-    int D = (H - A + (N + (N / (N + 1) * (1 - N) + (N == 0))) - 1) / (N + (N / (N + 1) * (1 - N) + (N == 0))) + 1; 
-    D = (D + 1) / 2 + (D) / 2; 
- 
-    printf("%d", D); 
-    return 0; 
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int h = 0;
+    int a = 0;
+    int b = 0;
+    int d = 0;
+
+    scanf_s("%d", &h);
+    scanf_s("%d", &a);
+    scanf_s("%d", &b);
+
+    int f = (a - b);
+    int g = h % f;
+    int k = (f - g) % f;
+
+    h += k;
+
+    int i = h - a;
+    int j = h / a;
+    int l = i / a;
+
+    h = i * (j - l);
+
+    d++;
+
+    h += ((a - b) - h % (a - b)) % (a - b);
+
+    d += h / (a - b);
+
+    printf("%d", d);
+
+    return EXIT_SUCCESS;
 }
