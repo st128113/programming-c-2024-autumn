08b506b8275f44d4da8659b4243febde77b3f52f
st128113 <st128113@student.spbu.ru>
2024-09-29 00:18:51 +0300

    HW

--- 2024.09.24-Homework-2/Task6/Source.cpp
+++ 2024.09.24-Homework-2/Task6/Source.cpp
@@ -1,50 +1,50 @@
-#include<iostream>
+#include <iostream>
 #include <math.h>
 
 int main(int argc, char* argv[])
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    scanf("%d", &c);
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    float d = 0;
+    float e = 0;
+    float f = 0;
+	scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
 
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
-                printf("-1\n");
-            }
-            else {
-                printf("0\n");
-            }
+        if (a != 0)
+    {
+        d = b * b - 4 * a * c;
+        if (d > 0)
+        {
+            e = (-b + sqrt(d)) / (2 * a);
+            f = (-b - sqrt(d)) / (2 * a);
+            printf("2 %.6f %.6f\n", e, f);
         }
-        else {
-            double x = -c / (double)b;
-            printf("1\n");
-            printf("%.6f\n", x);
+        else if (d == 0)
+        {
+            e = -b / (2 * a);
+            printf("1 %.6f\n", e);
+        }
+        else
+        {
+            printf("0\n");
         }
     }
-    else {
-        double d = b * b - 4 * a * c;
-        if (d > 0) {
-            double sqrt_d = sqrt(d);
-            double x1 = (-b + sqrt_d) / (2 * a);
-            double x2 = (-b - sqrt_d) / (2 * a);
-            printf("2\n");
-            printf("%.6f\n", x1);
-            printf("%.6f\n", x2);
-        }
-        else if (d == 0) {
-            double x = -b / (2 * a);
-            printf("1\n");
-            printf("%.6f\n", x);
-        }
-        else {
-            printf("-1\n");
-        }
+    else if (b != 0)
+    {
+        e = -c / b;
+        printf("1 %.6f\n", e);
     }
-
-
+    else if (c == 0)
+    {
+        printf("-1\n");
+    }
+    else
+    {
+        printf("0\n");
+    }
+    
     return EXIT_SUCCESS;
 }
