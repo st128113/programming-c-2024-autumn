e1ff09910c1ebd403a0d2d7deb51ae3de3f46eec
st128113 <st128113@student.spbu.ru>
2024-10-13 13:19:41 +0300

    HW

--- 2024.10.01-Homework-4/Task2/Source.cpp
+++ 2024.10.01-Homework-4/Task2/Source.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
     int n = 0;
     scanf("%d", &n);
-    int a[1000];
+    int a[1000] = { 0 };
 
     for (int i = 0; i < n; ++i) {
         scanf("%d", &a[i]);
